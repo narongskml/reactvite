c948289ba21a40eb9ab3fcfc7e9235851b4d7eea
Narong Sungkhamalai <narong.skml@gmail.com>
2023-08-06 22:27:13 +0700

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import NavBar from './components/NavBar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar/>
       <h1>Hi Github Page</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
