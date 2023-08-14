5cf1cc8a567a23f344eaf101ac7bc260c6ed68a8
Narong Sungkhamalai <narong.skml@gmail.com>
2023-08-14 19:55:53 +0700

    add flatpickr

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+
 import './App.css'
 import NavBar from './components/NavBar'
+import "flatpickr/dist/themes/material_green.css";
+
+import Flatpickr from "react-flatpickr";
 function App() {
   const [count, setCount] = useState(0)
+  const [date, setDate] = useState(new Date());
 
   return (
     <>
+      
       <NavBar/>
       <h1>Hi Github Page</h1>
       <div className="card">
@@ -15,6 +21,16 @@ function App() {
         <p>
           This is github page from Vite
         </p>
+      </div>
+      <div>
+        <p>This test Flatpickr </p>
+      <Flatpickr
+        data-enable-time
+        value={date}
+        onChange={() => {
+          setDate((date)=>date);
+        }}
+      />
       </div>
       <p>source code on <a href="https://github.com/narongskml/reactvite/">link</a> </p>
     </>
